80570864dd93bb70f70127086544cf008fc5211a
David <david.daraujo@gmail.com>
2019-03-07 16:20:24 -0800

    Starting Register Activity Implementation
    Just configuring and creating the GUI

--- app/src/main/java/ca/douglascollege/eatnow/Register.java
+++ app/src/main/java/ca/douglascollege/eatnow/Register.java
@@ -2,6 +2,8 @@ package ca.douglascollege.eatnow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
 
@@ -10,6 +12,13 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        //TODO: Create something here
+        final EditText etEmail = findViewById(R.id.etEmail);
+        final EditText etPassword = findViewById(R.id.etPassword);
+        final EditText etConfirmPassword = findViewById(R.id.etConfirmPasword);
+        final EditText etName = findViewById(R.id.etFirstName);
+        final EditText etLastName = findViewById(R.id.etLastName);
+        final EditText etAddress = findViewById(R.id.etAddress);
+        final EditText etEmailRecommend = findViewById(R.id.etEmailRecommend);
+        final Button btnSubmit = findViewById(R.id.btnSubmit);
     }
 }
